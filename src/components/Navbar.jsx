6b80169dd8c05c82ddd575475c2427106e04a412
Meghana-Ravalkol <ravalkolmeghana@gmail.com>
2026-05-19 09:27:02 +0530

    fixed logo import

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import {Link} from "react-router-dom"
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './Navbar.css'
-import logo from './src/assets/logo1.png'
+import logo1 from '../assets/logo1.png'
 function Navbar(){
     return (
         <div className="navbar">
-            <img src={logo} alt="logo"/>
+            <img src={logo1} alt="logo"/>
             <ul>
                 <li><Link to='/'>Home</Link></li>
                 <li><Link to='/add'>Add</Link></li>
